3bc527476b094f9166411af6c388144b625461a6
github username <github email address>
2021-06-14 15:58:56 -0400

    commit tree 46a84ddc3ac76665835143a5f342804d564ee85a

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce((accumulator, cV) => {
+    return accumulator + cV}, 0)
+
+
+
+//const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+//const totalBatteries = batteryBatches.reduce(reducer)
+
+
